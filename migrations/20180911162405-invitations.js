f8b1fbcb5c38e3ce54924bd9c43d06f268f1d146
Gael Bau <gaerbau@gmail.com>
2018-09-11 09:32:07 -0700

    created migration for invitations table

--- migrations/20180911162405-invitations.js
+++ migrations/20180911162405-invitations.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const table = 'invitations'
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
@@ -9,6 +10,39 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
+    return queryInterface.createTable(table, {
+      senderId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onDelete: 'cascade'
+      },
+      receiverId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onDelete: 'cascade'
+      },
+      teamId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id'
+        },
+        onDelete: 'cascade'
+      },
+      createdOn: {
+        type: Sequelize.DATE, 
+        defaultValue: Sequelize.NOW
+      }
+    })
+    .then(() => {
+      return queryInterface.addIndex(table, ['createdOn'], {indexType:'BTREE'});
+    });
   },
 
   down: (queryInterface, Sequelize) => {
@@ -19,5 +53,6 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
+    return queryInterface.dropTable(table);
   }
 };
